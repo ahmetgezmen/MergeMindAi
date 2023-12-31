a859def58885ff48c998efc26c0ef13c2cb539b6
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2023-12-31 18:03:05 +0300

    add splash screen widget

--- lib/core/base/application_base.dart
+++ lib/core/base/application_base.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs
 import 'package:flutter/material.dart';
+import 'package:merge_mind_ai/middlewares/splash_screeen.dart';
 import 'package:merge_mind_ai/utils/enums/theme_mod_string_enum.dart';
 import 'package:merge_mind_ai/utils/helper/theme_mode_helper.dart';
 import 'package:merge_mind_ai/utils/theme/dark_theme.dart';
@@ -16,8 +17,7 @@ class MergeMindAI extends StatelessWidget {
         themeMode: ThemeModeHelper.getThemeMode(themeMode),
         darkTheme: CustomDarkThemes.darkTheme,
         theme: CustomLightThemes.lightTheme,
-        home: const Placeholder(),
-        // todo add splash screen  == > const SplashScreen(),
+        home: const SplashScreen(),
         );
   }
 }
---
+++ lib/middlewares/splash_screeen.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: public_member_api_docs
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
